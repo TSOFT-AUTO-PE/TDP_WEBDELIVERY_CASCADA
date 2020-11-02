5b2373289647ab12dc244a2ad80ccd85e6203688
jets.pucho <tdp.tsoft.qa@gmail.com>
2020-11-02 09:15:35 -0500

    actualizado

---
+++ src/main/java/com/tsoft/bot/frontend/steps/WebDelivery/CargaDeMateriales.java
@@ -0,0 +1,58 @@
+package com.tsoft.bot.frontend.steps.WebDelivery;
+
+import com.tsoft.bot.frontend.pages.pages.WebDelivery;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+
+public class CargaDeMateriales {
+    public WebDriver driver;
+    WebDelivery createCorPed = new WebDelivery();
+
+    @Given("^Se da click en el boton IR A en WEB DELIVERY \"([^\"]*)\"$")
+    public void seDaClickEnElBotonIRAEnWEBDELIVERY(String casoPrueba) throws Throwable {
+        createCorPed.seDaClickEnElBotonIRAEnWEBDELIVERY(casoPrueba);
+    }
+    @When("^seleccionar ajuste de inventario$")
+    public void seleccionarAjusteDeInventario() throws Exception {
+        createCorPed.seleccionarAjusteDeInventario();
+    }
+
+    @And("^click en el boton nuevo registro$")
+    public void clickEnElBotonNuevoRegistro() throws Exception {
+        createCorPed.clickEnElBotonNuevoRegistro();
+
+    }
+
+    @And("^seleccionamos el tipo ABASTECIMIENTO \"([^\"]*)\"$")
+    public void seleccionamosElTipoABASTECIMIENTO(String casoPrueba) throws Throwable {
+        createCorPed.seleccionamosElTipoABASTECIMIENTO(casoPrueba);
+    }
+    @And("^ingresamos un comentario \"([^\"]*)\"$")
+    public void ingresamosUnComentario(String casoDePrueba) throws Throwable {
+
+        createCorPed.ingresamosUnComentario(casoDePrueba);
+    }
+    @And("^ingresamos guia de remision \"([^\"]*)\"$")
+    public void ingresamosGuiaDeRemision(String casoDePrueba) throws Throwable {
+        createCorPed.ingresamosGuiaDeRemision(casoDePrueba);
+    }
+    @And("^ingresamos el archivo$")
+    public void ingresamosElArchivo() throws Throwable {
+
+   createCorPed.ingresamosElArchivo();
+
+    }
+    @And("^click en ejecutar ajuste y aceptar mensaje$")
+    public void clickEnEjecutarAjusteYAceptarMensaje() throws Exception {
+        createCorPed.clickEnEjecutarAjusteYAceptarMensaje();
+
+    }
+    @And("^validar que los archivos hayan cargado$")
+    public void validarQueLosArchivosHayanCargado() throws Exception {
+        createCorPed.validarQueLosArchivosHayanCargado();
+    }
+
+}
